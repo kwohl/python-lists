3025c3171d7e4d40f3c27949ffe5b6a4513d2513
kwohl <ktwohl@gmail.com>
2020-04-11 14:51:56 -0500

    planet list challenge exercise

--- planets.py
+++ planets.py
@@ -1,3 +1,4 @@
+# Practice: Planet List
 planet_list = ["Mercury", "Mars"]
 # use append() to add Jupiter and Saturn to the end of the list
 planet_list.append("Jupiter")
@@ -14,4 +15,43 @@ rocky_planets = planet_list[:4]
 # We know Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list
 del planet_list[-1]
 
+# Challenge: Iterating over Planets
 
+spacecrafts = [
+    ("Cassini", "Saturn"),
+    ("Viking", "Mars"),
+    ("Curiosity", "Mars"),
+    ("Galileo", "Jupiter"),
+    ("New Horizons", "Pluto"),
+    ("Voyager 2", "Jupiter", "Saturn", "Neptune", "Uranus"),
+    ("MESSENGER", "Mercury"),
+    ("Pioneer", "Venus"),
+    ("Space Craft", "Saturn")
+]
+
+# Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which satellites have visited it.
+
+
+for planet in planet_list:
+    planet_crafts = []
+    crafts = ""
+
+    for craft_tuple in spacecrafts:
+        if planet in craft_tuple:
+            planet_crafts.append(craft_tuple[0])
+
+    for planet_craft in planet_crafts:
+        if (planet_crafts.index(planet_craft) < (len(planet_crafts)-1)) and len(planet_crafts) > 2:
+            crafts = crafts + planet_craft + ', '
+        elif (planet_crafts.index(planet_craft) >= len(planet_crafts)-1) and len(planet_crafts) > 2:
+            crafts = crafts + 'and ' + planet_craft + '.'
+        elif len(planet_crafts) == 1:
+            crafts = crafts + planet_craft + '.'
+        
+    if len(planet_crafts) == 2:
+        crafts = planet_crafts[0] + ' and ' + planet_crafts[1] + '.'
+    if len(planet_crafts) == 0:
+        crafts = "nobody."
+
+    print(f"{planet} was visited by {crafts}")
+    # print(f"{planet} was visited by {' and '.join(planet_crafts)}")
